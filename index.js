8ecc7a8cb8b08d89329acd1a0008b2b7c3fd9a76
MRN0B0DY045 <56263415+MRN0B0DY045@users.noreply.github.com>
2020-10-09 23:30:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on("ready", async () => {
     console.log(`${client.user.username} is online`) 
-    client.user.setActivity('scripts', {type: "TESTING"})
+    client.user.setActivity('this server', {type: "WATCHING"})
 })
 
 
